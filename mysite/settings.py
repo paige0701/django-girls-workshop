e711cc2e4a22f8a941a2383e39a98755cc0087d1
exntu.metatron <cde0701@gmail.com>
2018-07-21 14:38:37 +0900

    add base template
    add css
    
    add post detail page

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a#w_t7elc4%4&_@au=ly8o!w7$bde#3o!tgg4a^uy0y@aam#6('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
